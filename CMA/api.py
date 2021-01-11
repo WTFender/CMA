ba5326690b15b152519bc8afc0690de5721a1fe3
WTFender <wtfender.cs@gmail.com>
2021-01-10 21:51:27 -0800

    add api options

--- CMA/api.py
+++ CMA/api.py
@@ -1,4 +1,9 @@
 import requests
+import urllib
+
+
+# https://openaccess-api.clevelandart.org/
+
 
 class Handler:
 
@@ -6,9 +11,9 @@ class Handler:
         self.baseurl = baseurl
         self.key = key # unimplemented
 
-    def _api(self, method, path, payload):
-        if payload:
-            resp = requests.request(method, self.baseurl+path, json=payload)
+    def _api(self, method, path, params):
+        if params:
+            resp = requests.request(method, self.baseurl+path, params=params)
         else:
             resp = requests.request(method, self.baseurl+path)
         if resp.status_code != 200:
@@ -16,8 +21,98 @@ class Handler:
         else:
             return resp.json()['data']
 
-    def get_artworks(self):
-        return self._api('GET', '/artworks', None)
+    def get_artworks(self, **kwargs):
+        '''
+        q	string	Any keyword or phrase that searches against title, creator, artwork description, and several other meaningful fields related to the artwork.
+        department	string	Filter by department. List of valid departments in Appendix B.
+        type	string	Filter by artwork types. List of valid types in Appendix C.
+        has_image	integer	0 or 1. Filter to return only artworks that have a web image asset. (synonymous with the deprecated field web_image)
+        indent	integer	Number of spaces to indent JSON content if "pretty" formatting is desired.
+        skip	integer	Offset index for results.
+        limit	integer	Limit for number of results. If no limit provided, API will return the maximum (1000) number of records.
+        artists	string	Filter by name of artist.
+        title	string	Filter by title of artwork.
+        medium	string	Filter by artwork medium.
+        dimensions	float,float,float	Filter artworks by dimensions with the unit of measurement being meters. This filter is somewhat tricky, as the terminolgy for describing object dimensions varies from object to object (for example coins have diameters, swords have lengths, and necklaces have heights). An object's most descriptive dimension (whatever you think is the best way to describe it in meters) is generally put in the first part of the comma seperated list of dimensions. A default value of 20cm will be used if no value is provided for a dimension in the list. The second and third dimensions places are interchangable and describe a square that an object's remaining dimensions could fit inside. The dimensions filter returns objects with a fault tolerance of 20cm on all dimensions.
+        dimensions_max	float,float,float	Filter artworks to return all works that can fit inside a box defined by provided 3 values with the unit of measurement being meters. Place the most descriptive dimension in the first value, and any remaining dimensions in the second two values. If no value is provided for a dimension, a default value of 20cm is used. The dimensions_max filter has a fault tolerance of 0 on all dimensions, and will not return objects that cannot fit in the described box.
+        dimensions_min	float,float,float	Filter artworks to return all works that cannot fit inside a box defined by provided 3 values with the unit of measurement being meters. Place the most descriptive dimension in the first value, and any remaining dimensions in the second two values. If no value is provided for a dimension, a default value of 20cm is used. The dimensions_min filter has a fault tolerance of 0 on all dimensions, and will not return objects that can fit in the described box.
+        credit	string	Filter by credit line.
+        catalogue_raisonne	string	Filter by catalogue raisonne.
+        provenance	string	Filter by provenance of artwork
+        citations	string	Keyword search against the citations field.
+        exhibition_history	string	Filter by exhibition history of artwork.
+        created_before	integer	Returns artworks created before the year specified. Negative years are BCE.
+        created_after	integer	Returns artworks created after the year specified. Negative years are BCE.
+        created_after_age	integer	Filters by artworks that were created by artists older than the provided value in years at time of creation.
+        created_before_age	integer	Filters by artworks that were created by artists younger than the provided value in years at time of creation.      
+        cc0	none	Filters by works that have share license cc0.
+        copyrighted	none	Filters by works that have some sort of copyright.
+        currently_on_view	none	Filters by works that are currently on view at CMA.
+        currently_on_loan	none	Filters by works that are currently on loan.
+        african_american_artists	none	Filters by works created by African American artists.
+        cia_alumni_artists	none	Filters by works created by Cleveland Institute of Art alumni.
+        may_show_artists	none	Filters by works exhibited in Cleveland Museum of Art May Shows
+        female_artists	none	Filters by artworks created by female artists.
+        recently_acquired	none	Filters by artworks acquired by the museum in the last three years.
+        nazi_era_provenance	none	Filters by nazi-era provenance.
+        '''
+        params = {}
+        if kwargs['q']:
+            params['q'] = str(kwargs['q'])
+        if kwargs['department']:
+            params['department'] = str(kwargs['department'])
+        if kwargs['type']:
+            params['type'] = str(kwargs['type'])
+        if kwargs['has_image']:
+            params['has_image'] = int(kwargs['has_image'])
+        if kwargs['indent']:
+            params['indent'] = int(kwargs['indent'])
+        if kwargs['skip']:
+            params['skip'] = int(kwargs['skip'])
+        if kwargs['limit']:
+            params['limit'] = int(kwargs['limit'])
+        if kwargs['artists']:
+            params['artists'] = str(kwargs['artists'])
+        if kwargs['title']:
+            params['title'] = str(kwargs['title'])
+        if kwargs['medium']:
+            params['medium'] = str(kwargs['medium'])
+        if kwargs['dimensions']:
+            params['dimensions'] = str(kwargs['dimensions'])
+        if kwargs['dimensions_max']:
+            params['dimensions_max'] = str(kwargs['dimensions_max'])
+        if kwargs['dimensions_min']:
+            params['dimensions_min'] = str(kwargs['dimensions_min'])
+        if kwargs['credit']:
+            params['credit'] = str(kwargs['credit'])
+        if kwargs['catalogue_raisonne']:
+            params['catalogue_raisonne'] = str(kwargs['catalogue_raisonne'])
+        if kwargs['provenance']:
+            params['provenance'] = str(kwargs['provenance'])
+        if kwargs['citations']:
+            params['citations'] = str(kwargs['citations'])
+        if kwargs['exhibition_history']:
+            params['exhibition_history'] = str(kwargs['exhibition_history'])
+        if kwargs['created_before']:
+            params['created_before'] = int(kwargs['created_before'])
+        if kwargs['created_after']:
+            params['created_after'] = int(kwargs['created_after'])
+        if kwargs['created_after_age']:
+            params['created_after_age'] = int(kwargs['created_after_age'])
+        if kwargs['created_before_age']:
+            params['created_before_age'] = int(kwargs['created_before_age'])
+
+        path = ''
+        for k in ['cc0','copyrighted','currently_on_view','currently_on_loan',
+                'african_american_artists','cia_alumni_artists','may_show_artists',
+                'female_artists','recently_acquired','nazi_era_provenance']:
+            if k in kwargs:
+                if kwargs[k]:
+                    path += k + '&'
+
+        path = path[:-1] # remove trailing &
+
+        return self._api('GET', '/artworks/?' + path, params)
 
     def get_artwork(self, rid, preview=False):
         rid = str(rid)
--- CMA/cli.py
+++ CMA/cli.py
@@ -14,13 +14,47 @@ exhibit_cmd = subparsers.add_parser("exhibition")
 
 # Require --id or --search for each subcommand
 for cmd in [artwork_cmd, curator_cmd, exhibit_cmd]:
-    cmd.add_argument('--pretty', dest='pretty', action='store_true', help='pretty print summary')
     flags = cmd.add_mutually_exclusive_group(required=True)
     flags.add_argument('--search', dest='search', type=str, help='search terms for resource')
     flags.add_argument('--id', dest='resource', type=str, help='resource ID to retrieve')
 
+# Add api options
+artwork_cmd.add_argument('--query', '-q', dest='q', type=str, help='Any keyword or phrase that searches against title, creator, artwork description, and several other meaningful fields related to the artwork.')
+artwork_cmd.add_argument('--department', dest='department', type=str, help='Filter by department. List of valid departments in Appendix B.')
+artwork_cmd.add_argument('--type', dest='type', type=str, help='Filter by artwork types. List of valid types in Appendix C.')
+artwork_cmd.add_argument('--has_image', dest='has_image', type=int, help='0 or 1. Filter to return only artworks that have a web image asset. (synonymous with the deprecated field web_image)')
+artwork_cmd.add_argument('--indent', dest='indent', type=int, help='Number of spaces to indent JSON content if "pretty" formatting is desired.')
+artwork_cmd.add_argument('--skip', dest='skip', type=int, help='Offset index for results.')
+artwork_cmd.add_argument('--limit', dest='limit', type=int, help='Limit for number of results. If no limit provided, API will return the maximum (1000) number of records.')
+artwork_cmd.add_argument('--artists', dest='artists', type=str, help='Filter by name of artist.')
+artwork_cmd.add_argument('--title', dest='title', type=str, help='Filter by title of artwork.')
+artwork_cmd.add_argument('--medium', dest='medium', type=str, help='Filter by artwork medium.')
+artwork_cmd.add_argument('--dimensions', dest='dimensions', type=str, help='Filter artworks by dimensions with the unit of measurement being meters. This filter is somewhat tricky, as the terminolgy for describing object dimensions varies from object to object (for example coins have diameters, swords have lengths, and necklaces have heights). An object\'s most descriptive dimension (whatever you think is the best way to describe it in meters) is generally put in the first part of the comma seperated list of dimensions. A default value of 20cm will be used if no value is provided for a dimension in the list. The second and third dimensions places are interchangable and describe a square that an object\'s remaining dimensions could fit inside. The dimensions filter returns objects with a fault tolerance of 20cm on all dimensions.')
+artwork_cmd.add_argument('--dimensions_max', dest='dimensions_max', type=str, help='Filter artworks to return all works that can fit inside a box defined by provided 3 values with the unit of measurement being meters. Place the most descriptive dimension in the first value, and any remaining dimensions in the second two values. If no value is provided for a dimension, a default value of 20cm is used. The dimensions_max filter has a fault tolerance of 0 on all dimensions, and will not return objects that cannot fit in the described box.')
+artwork_cmd.add_argument('--dimensions_min', dest='dimensions_min', type=str, help='Filter artworks to return all works that cannot fit inside a box defined by provided 3 values with the unit of measurement being meters. Place the most descriptive dimension in the first value, and any remaining dimensions in the second two values. If no value is provided for a dimension, a default value of 20cm is used. The dimensions_min filter has a fault tolerance of 0 on all dimensions, and will not return objects that can fit in the described box.')
+artwork_cmd.add_argument('--credit', dest='credit', type=str, help='Filter by credit line.')
+artwork_cmd.add_argument('--catalogue_raisonne', dest='catalogue_raisonne', type=str, help='Filter by catalogue raisonne.')
+artwork_cmd.add_argument('--provenance', dest='provenance', type=str, help='Filter by provenance of artwork')
+artwork_cmd.add_argument('--citations', dest='citations', type=str, help='Keyword search against the citations field.')
+artwork_cmd.add_argument('--exhibition_history', dest='exhibition_history', type=str, help='Filter by exhibition history of artwork.')
+artwork_cmd.add_argument('--created_before', dest='created_before', type=int, help='Returns artworks created before the year specified. Negative years are BCE.')
+artwork_cmd.add_argument('--created_after', dest='created_after', type=int, help='Returns artworks created after the year specified. Negative years are BCE.')
+artwork_cmd.add_argument('--created_after_age', dest='created_after_age', type=int, help='Filters by artworks that were created by artists older than the provided value in years at time of creation.')
+artwork_cmd.add_argument('--created_before_age', dest='created_before_age', type=int, help='Filters by artworks that were created by artists younger than the provided value in years at time of creation.')
+artwork_cmd.add_argument('--cc0', dest='cc0', action='store_true', help='Filters by works that have share license cc0.')
+artwork_cmd.add_argument('--copyrighted', dest='copyrighted', action='store_true', help='Filters by works that have some sort of copyright.')
+artwork_cmd.add_argument('--currently_on_view', dest='currently_on_view', action='store_true', help='Filters by works that are currently on view at CMA.')
+artwork_cmd.add_argument('--currently_on_loan', dest='currently_on_loan', action='store_true', help='Filters by works that are currently on loan.')
+artwork_cmd.add_argument('--african_american_artists', dest='african_american_artists', action='store_true', help='Filters by works created by African American artists.')
+artwork_cmd.add_argument('--cia_alumni_artists', dest='cia_alumni_artists', action='store_true', help='Filters by works created by Cleveland Institute of Art alumni.')
+artwork_cmd.add_argument('--may_show_artists', dest='may_show_artists', action='store_true', help='Filters by works exhibited in Cleveland Museum of Art May Shows')
+artwork_cmd.add_argument('--female_artists', dest='female_artists', action='store_true', help='Filters by artworks created by female artists.')
+artwork_cmd.add_argument('--recently_acquired', dest='recently_acquired', action='store_true', help='Filters by artworks acquired by the museum in the last three years.')
+artwork_cmd.add_argument('--nazi_era_provenance', dest='nazi_era_provenance', action='store_true', help='Filters by nazi-era provenance.')
+
 # Add ascii preview flag
 artwork_cmd.add_argument('--preview', dest='preview', action='store_true', help='generate ascii preview')
+
 
 def main():
     cma = Handler()
@@ -34,10 +68,7 @@ def main():
         if args.resource:
             output = cma.get_artwork(rid=args.resource, preview=args.preview)
 
-            if args.pretty:
-                print()
-
-            if args.pretty:
+            if args.preview:
                 print('Title: ' + output['title'])
                 print('Type: ' + output['type'])
                 creators = ", ".join([c['description'] for c in output['creators']])
@@ -49,7 +80,10 @@ def main():
                     print(output['preview'])
 
         elif args.search:
-            output = 'search not yet implemented'
+            output = cma.get_artworks(**args.__dict__)
+
+            if args.preview:
+                print('No. Results: ' + str(len(output)))
 
     elif args.command == 'curator':
         output = 'curator not yet implemented'
@@ -60,5 +94,5 @@ def main():
     else:
         raise Exception('Unknown subcommand.')
     
-    if not args.pretty:
+    if not args.preview:
         print(json.dumps(output, indent=4))
